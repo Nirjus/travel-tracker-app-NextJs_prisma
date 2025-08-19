f12d60db7930d1b04df03f76aa0157592a5ceef0
Nirjus <karmakarnirjus4839@gmail.com>
2025-08-19 14:43:26 +0530

    implementing Jwt based authentication register and login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { LoginDto, RegisterDto } from './dto/register.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
   // handle new user registration
-  async register() {
+  async register(registerDto: RegisterDto) {
+    const { email, password } = registerDto;
     // check if user is exists or not
+    const existingUser = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (existingUser) {
+      throw new ConflictException(
+        'User already exists! Please try with a different email',
+      );
+    }
     // hash the password
-    // create password from the return object
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // create user in the database and return the user object without password
+    const newlyCreatedUser = await this.prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+    const { password: _, ...result } = newlyCreatedUser;
+    return result;
+  }
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    // find currentuser by email
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials! try again');
+    }
+
+    // verify the password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Wrong password, try again');
+    }
+    const token = this.jwtService.sign({ userId: user.id });
+
+    const { password: _, ...result } = user;
+
+    return { ...result, token };
   }
 }
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,9 +1,19 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
   @IsEmail({}, { message: 'Please provide a valid email address' })
   email: string;
 
-  @IsString()
+  @IsString({ message: 'Password must ne a string' })
+  @MinLength(6, { message: 'Password must be 6 character long' })
+  password: string;
+}
+
+export class LoginDto {
+  @IsEmail({}, { message: 'Please provide a valid email address' })
+  email: string;
+
+  @IsString({ message: 'Password must ne a string' })
+  @MinLength(6, { message: 'Password must be 6 character long' })
   password: string;
 }
